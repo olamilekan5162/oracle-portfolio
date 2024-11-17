53cd904c5964de08d60a5f500c3159b557266c7c
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-11-17 09:04:39 +0100

    setting up components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return ( 
+        <div className="nav-container">
+            <h1>WELCOME TO ORACLE PORTFOLIO</h1>
+            <a href="#">oracle</a>
+        </div>
+     );
+}
+ 
+export default Navbar;
