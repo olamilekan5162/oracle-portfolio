418b09381420cf6572509dfad5a21421383cf940
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-11-02 11:26:59 +0100

    scaffold new porfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,34 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import Profile from './components/GitHubProfile'
-import Skills from './components/Skills'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-
-import night from './assets/night.svg'
-import day from './assets/day.svg'
-
-const App = () => {
-  const [githubUsers, setGithubUsers] = useState(null)
-  const [error, setError] = useState(null)
-  const [mode, setMode] = useState(night);
-  
-  const toggleMode = () => {
-    if (mode === night){
-      setMode(day)
-      alert('wiiiiii 😂😝 enjoy your dark mode, i no understand the use context jare😔')
-    }else {
-      setMode(night)
-      alert('wiiiiii 😂😝 enjoy your day mode i no understand the use context seh 😔')
-    }
-  }
-  
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try{
-        const res = await fetch('https://api.github.com/users?per_page=5');
-        if (!res.ok){
-          throw new Error ('Cannot fetch github Users');
-        }
-        const data = await res.json();
-        console.log(data)
-        setGithubUsers(data);    
-      }
-      catch (e){
-        setError(e.message)
-      }
-    }
-    fetchUsers()
-  },[])
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className={mode === night ? 'main-container-day' : 'main-container-night'}>
-      <Navbar mode = {mode} toggleMode = {toggleMode} />
-      <Hero />
-      {error && <div className="error">{error}</div>}
-      { githubUsers && <Profile githubUsers = {githubUsers}/>}
-      <Skills />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return ( 
-        <div className="footer-container">
-            Created by Oracle &copy; 2024
-        </div>
-     );
-}
- 
-export default Footer;
